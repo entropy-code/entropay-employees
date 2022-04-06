504827c5bd8ff24913a78e6a4574248001047dba
Leonardo Carrasco <lcarrasco.dev@gmail.com>
2022-04-06 17:50:36 -0300

    Add dockerfile (#1)
    Update pom.xml
    Set server port to 8100
    Other changes:
    - Addressed requested changes from PR.

--- src/main/java/com/entropyteam/entropay/employees/EntropayEmployeesApplication.java
+++ src/main/java/com/entropyteam/entropay/employees/EntropayEmployeesApplication.java
@@ -1,4 +1,4 @@
-package com.entropy.entropay.employees;
+package com.entropyteam.entropay.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
