76999dfea91e300abb483af8bdb25f558d2425c9
Agustin Bourgeois <abourgeois@workstride.com>
2022-06-02 19:45:35 -0300

    Get release version

--- src/main/java/com/entropyteam/entropay/employees/AppController.java
+++ src/main/java/com/entropyteam/entropay/employees/AppController.java
@@ -1,5 +1,7 @@
 package com.entropyteam.entropay.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+    @Autowired
+    BuildProperties buildProperties;
+
     @GetMapping("/health-check")
     public ResponseEntity<String> heathCheck(){
-        return ResponseEntity.ok("Entropay-employess: OK, Version: 2");
+        return ResponseEntity.ok("Entropay-employess: OK, Version: 2, " + buildProperties.getVersion());
     }
 }
 
