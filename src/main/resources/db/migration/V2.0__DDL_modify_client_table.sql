402c31817d69edaf27e4bbad6d7aa505d07b0f79
Agustin Bourgeois <agustinb4@gmail.com>
2022-08-16 20:50:56 -0300

    Fix cd/ci scripts (#13)
    * fix sql scripts to use public schema
    
    * fix deploy script
    
    Co-authored-by: Agustin Bourgeois <abourgeois@workstride.com>

--- src/main/resources/db/migration/V2.0__DDL_modify_client_table.sql
+++ src/main/resources/db/migration/V2.0__DDL_modify_client_table.sql
@@ -1,15 +1,15 @@
-ALTER TABLE employees.clients
+ALTER TABLE clients
     RENAME COLUMN created_on TO created_at;
 
-ALTER TABLE employees.clients
+ALTER TABLE clients
     RENAME COLUMN modified_on TO modified_at;
 
-ALTER TABLE employees.clients
+ALTER TABLE clients
     RENAME COLUMN is_active TO deleted;
 
-ALTER TABLE employees.clients
+ALTER TABLE clients
     ALTER COLUMN deleted SET DEFAULT FALSE;
 
-alter table employees.clients
+alter table clients
     rename to client;
 
