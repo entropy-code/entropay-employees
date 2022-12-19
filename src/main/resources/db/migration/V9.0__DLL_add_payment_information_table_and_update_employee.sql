efd551e8348dd8a3778dd2c5119a8b10ddab35c2
Facundo <facundo.ore@hotmail.com>
2022-12-19 10:27:59 -0300

    sintax error fixed in db migration

--- src/main/resources/db/migration/V9.0__DLL_add_payment_information_table_and_update_employee.sql
+++ src/main/resources/db/migration/V9.0__DLL_add_payment_information_table_and_update_employee.sql
@@ -18,7 +18,7 @@ CREATE TABLE payment_information(
                                     cbu         VARCHAR(255),
                                     created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
                                     modified_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-                                    deleted  BOOLEAN NOT NULL
+                                    deleted  BOOLEAN NOT NULL,
                                     CONSTRAINT payment_information_pk PRIMARY KEY (id),
                                     CONSTRAINT payment_information_employee_fk FOREIGN KEY (employee_id) REFERENCES employee(id)
 );
