8ed39bee758f058e2604d2534b471c06f49b53b2
Facundo <facundo.ore@hotmail.com>
2023-02-02 11:17:14 -0300

    EN-55: menu update, fix db script

---
+++ src/main/resources/db/migration/V20.1__DDL_update_menu.sql
@@ -0,0 +1,73 @@
+UPDATE config SET "role"='ROLE_DEVELOPMENT', permissions='[
+  {"entity": "employees", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "contracts", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "assignments", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "clients", "actions": ["create", "read", "update", "delete"]}
+]'::json::json, menu='[
+  {
+    "name": "Employees",
+    "href": "/#/employees",
+    "icon": "employees",
+    "key": 1
+  },
+  {
+    "name": "Contracts",
+    "href": "/#/contracts",
+    "icon": "contracts",
+    "key": 2
+  },
+  {
+    "name": "Assignments",
+    "href": "/#/assignments",
+    "icon": "assignments",
+    "key": 3
+  },
+  {
+    "name": "Clients",
+    "href": "/#/clients",
+    "icon": "clients",
+    "key": 4
+  },
+  {
+    "name": "Settings",
+    "icon": "settings",
+    "key": 5,
+    "submenu": [
+      {
+        "name": "Companies",
+        "href": "/#/companies",
+        "key": 51
+      },
+      {
+        "name": "Projects",
+        "href": "/#/projects",
+        "key": 52
+      },
+      {
+        "name": "Project types",
+        "href": "/#/project-types",
+        "key": 53
+      },
+      {
+        "name": "Roles",
+        "href": "/#/roles",
+        "key": 54
+      },
+      {
+        "name": "Seniorities",
+        "href": "/#/seniorities",
+        "key": 55
+      },
+      {
+        "name": "Technologies",
+        "href": "/#/technologies",
+        "key": 56
+      },
+      {
+        "name": "Tenants",
+        "href": "/#/tenants",
+        "key": 57
+      }
+    ]
+  }
+]'::json::json, created_at='2022-12-28 15:19:29.000', modified_at='2022-12-28 15:19:29.000', deleted=false WHERE id='3c245af9-19fc-4e02-b9b0-a85e8ed06594'::uuid;
