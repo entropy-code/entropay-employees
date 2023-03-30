5d102305a4e5490d7b9fdc24c78d5055ba5cc95f
Facundo <facundo.ore@hotmail.com>
2023-03-30 14:45:23 -0300

    fix code in db migration v25

--- src/main/resources/db/migration/V25.0__DDL_remove_trigger_from_contract.sql
+++ src/main/resources/db/migration/V25.0__DDL_remove_trigger_from_contract.sql
@@ -2,5 +2,5 @@ DROP FUNCTION on_modify_update_modified_at();
 
 DROP TRIGGER IF EXISTS unique_contract_active ON public.contract;
 
-DROP FUNCTION on_active_status_validate_unique();}
+DROP FUNCTION on_active_status_validate_unique();
 
