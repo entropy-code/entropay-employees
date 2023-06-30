81c60cfc9b51a5839ea1d14825f3accce7f5fb74
FacuOrellana <70980835+FacuOrellana@users.noreply.github.com>
2023-06-30 11:35:00 -0300

    EN-80: Set and see vacations per employee per year (#69)
    * EN-80: set vacations

---
+++ src/main/resources/db/migration/V29.0__DDL_create_vacation_table_update_permissions.sql
@@ -0,0 +1,58 @@
+CREATE TABLE vacation
+(
+    id          uuid
+        CONSTRAINT vacation_pk
+            PRIMARY KEY,
+    year        varchar(100) NOT NULL,
+    credit      numeric      NULL,
+    debit       numeric      NULL,
+    employee_id UUID         NOT NULL,
+    created_at  timestamp    NOT NULL,
+    modified_at timestamp    NOT NULL,
+    deleted     boolean      NOT NULL,
+    CONSTRAINT vacation_fk FOREIGN KEY (employee_id) REFERENCES employee (id)
+)
+;
+--ROLE MANAGER_HR permissions
+UPDATE config SET permissions='[
+  {"entity": "employees", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "contracts", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "assignments", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "clients", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "vacations", "actions": ["create", "read", "update", "delete"]}
+]'::json::json WHERE id='b1bf3a8c-ced5-443f-90a3-ee9d283a18c0'::uuid;
+
+--ROLE ADMIN permissions
+UPDATE config SET permissions='[
+  {"entity": "employees", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "contracts", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "assignments", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "clients", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "companies", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "projects", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "project-types", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "roles", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "seniorities", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "technologies", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "tenants", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "leave-types", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "holidays", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "countries", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "vacations", "actions": ["create", "read", "update", "delete"]}
+]'::json::json WHERE id='d172386e-46b3-4c6f-b030-14b9204ea059'::uuid;
+
+--ROLE DEVELOPMENT permissions
+UPDATE config SET permissions='[
+  {"entity": "employees", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "contracts", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "assignments", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "clients", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "companies", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "projects", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "project-types", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "roles", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "seniorities", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "technologies", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "tenants", "actions": ["create", "read", "update", "delete"]},
+  {"entity": "vacations", "actions": ["create", "read", "update", "delete"]}
+]'::json::json WHERE id='3c245af9-19fc-4e02-b9b0-a85e8ed06594'::uuid;
