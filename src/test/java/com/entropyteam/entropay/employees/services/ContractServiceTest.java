6f8727e572517e49108d5905876da70157c82ea4
Marco <baremarco@gmail.com>
2023-07-01 08:31:12 -0300

    en-87 update flyway migration to add new column on employee table

--- src/test/java/com/entropyteam/entropay/employees/services/ContractServiceTest.java
+++ src/test/java/com/entropyteam/entropay/employees/services/ContractServiceTest.java
@@ -35,7 +35,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import com.entropyteam.entropay.auth.SecureObjectService;
