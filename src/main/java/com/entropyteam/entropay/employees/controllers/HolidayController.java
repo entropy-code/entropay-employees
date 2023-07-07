a561a9727fd26271439601bfa5152e3357fa1b6e
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-07-07 10:53:33 -0300

    EN-102: Tech debt on holidays component (#70)
    * EN-102: Tech debt on holidays component
    
    * EN-102:change from Object to Integer
    
    * EN-102: it was added in the HolidayRepository query
    WHERE deleted=false

--- src/main/java/com/entropyteam/entropay/employees/controllers/HolidayController.java
+++ src/main/java/com/entropyteam/entropay/employees/controllers/HolidayController.java
@@ -5,11 +5,15 @@ import com.entropyteam.entropay.employees.dtos.HolidayDto;
 import com.entropyteam.entropay.employees.services.HolidayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
@@ -20,8 +24,17 @@ import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
 @RequestMapping(value = "/holidays", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HolidayController extends BaseController<HolidayDto, UUID> {
 
+    private final HolidayService holidayService;
+
     @Autowired
     public HolidayController(HolidayService holidayService) {
         super(holidayService);
+        this.holidayService = holidayService;
+    }
+    @GetMapping("/years")
+    public ResponseEntity<List<Map<String, Integer>>> getHolidayYears() {
+        List<Map<String, Integer>> holidayYears = holidayService.getHolidayYears();
+        return ResponseEntity.ok().header(BaseController.X_TOTAL_COUNT, String.valueOf(holidayYears.size()))
+                .body(holidayYears);
     }
 }
--- src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
+++ src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
@@ -2,8 +2,13 @@ package com.entropyteam.entropay.employees.repositories;
 
 import com.entropyteam.entropay.common.BaseRepository;
 import com.entropyteam.entropay.employees.models.Holiday;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface HolidayRepository extends BaseRepository<Holiday, UUID> {
+
+    @Query(value = "SELECT DISTINCT extract('Year' FROM date) AS year FROM holiday_calendar WHERE deleted=false ORDER BY year ASC", nativeQuery = true)
+    List<Integer> getHolidaysYears();
 }
