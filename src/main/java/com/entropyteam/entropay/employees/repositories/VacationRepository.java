cc6cf416a2903f1389de78fcff83b731c2599e80
FacuOrellana <70980835+FacuOrellana@users.noreply.github.com>
2023-07-13 09:23:58 -0300

    EN-82: See available vacations days per employee (#71)
    * EN-82: see remaining vacations available per employee

--- src/main/java/com/entropyteam/entropay/employees/repositories/VacationRepository.java
+++ src/main/java/com/entropyteam/entropay/employees/repositories/VacationRepository.java
@@ -3,7 +3,12 @@ package com.entropyteam.entropay.employees.repositories;
 import java.util.UUID;
 import com.entropyteam.entropay.common.BaseRepository;
 import com.entropyteam.entropay.employees.models.Vacation;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VacationRepository extends BaseRepository<Vacation, UUID> {
+
+    @Query("SELECT CAST((SUM(v.credit) - COALESCE(SUM(v.debit), 0)) AS int) AS total_balance FROM Vacation v WHERE v.employee.id = :employeeId AND v.deleted = false GROUP BY v.employee.id")
+    Integer getAvailableDays(@Param("employeeId") UUID employeeId);
 
 }
