9fd68602c5c5eb26808b1c9fe934c582c4c7de79
FedericoA15 <federicoalmeida15@gmail.com>
2023-08-25 12:54:20 -0300

    EN-106:  change enum

--- src/main/java/com/entropyteam/entropay/auth/AppRole.java
+++ src/main/java/com/entropyteam/entropay/auth/AppRole.java
@@ -6,9 +6,9 @@ import org.apache.commons.lang3.StringUtils;
 public enum AppRole {
     ROLE_ADMIN("ROLE_ADMIN", 1),
     ROLE_DEVELOPMENT("ROLE_DEVELOPMENT", 2),
-    ROLE_MANAGER_HR("ROLE_MANAGER/HR", 3),
-    ROLE_ANALYST("ROLE_ANALYST", 4),
-    ROLE_HR_DIRECTOR("ROLE_DIRECTOR_HR", 5);
+    ROLE_HR_DIRECTOR("ROLE_HR_DIRECTOR", 3),
+    ROLE_MANAGER_HR("ROLE_MANAGER/HR", 4),
+    ROLE_ANALYST("ROLE_ANALYST", 5);
 
     public String value;
     public Integer score;
