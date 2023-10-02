e5714b610a7f6f19021882a540266a70291aca4b
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-10-02 10:29:59 -0300

    En 109: employees reporting (#100)
    * EN-109: Employee reporting
    
    * EN-106: employee reporting
    
    * EN-106: fix controller and service
    
    * EN-106: fix controller and new migration
    
    * EN-106: fix dto and migration
    
    ---------
    
    Co-authored-by: Facundo <facundo.ore@hotmail.com>

---
+++ src/main/java/com/entropyteam/entropay/employees/controllers/ReportController.java
@@ -0,0 +1,47 @@
+package com.entropyteam.entropay.employees.controllers;
+
+import com.entropyteam.entropay.common.BaseController;
+import com.entropyteam.entropay.employees.dtos.EmployeeReportDto;
+import com.entropyteam.entropay.employees.services.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_HR_DIRECTOR;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_DEVELOPMENT;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_MANAGER_HR;
+
+
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/reports", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+
+    @GetMapping("/employees")
+    @Secured({ROLE_ADMIN, ROLE_MANAGER_HR, ROLE_HR_DIRECTOR, ROLE_DEVELOPMENT})
+    @Transactional
+    public ResponseEntity<List<EmployeeReportDto>> getEmployeesReport() {
+        Page<EmployeeReportDto> response = reportService.getEmployeesReport();
+        return ResponseEntity.ok()
+                .header(BaseController.X_TOTAL_COUNT, String.valueOf(response.getTotalElements()))
+                .body(response.getContent());
+    }
+
+}
