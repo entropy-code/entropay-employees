298d8be53f2b8f40871eb9cfe0a71460bc075b9a
FedericoA15 <federicoalmeida15@gmail.com>
2023-12-04 16:51:16 -0300

    EN-130: fix import job

--- src/main/java/com/entropyteam/entropay/employees/jobs/EmployeeJob.java
+++ src/main/java/com/entropyteam/entropay/employees/jobs/EmployeeJob.java
@@ -2,6 +2,7 @@ package com.entropyteam.entropay.employees.jobs;
 
 import com.entropyteam.entropay.employees.dtos.CalendarEventDto;
 
+import com.entropyteam.entropay.employees.services.EmployeeService;
 import com.entropyteam.entropay.employees.repositories.EmployeeRepository;
 import com.entropyteam.entropay.employees.services.GoogleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,11 @@ import java.io.IOException;
 @Component
 public class EmployeeJob {
     private final EmployeeRepository employeeRepository;
-    private final com.entropyteam.entropay.employees.services.EmployeeService employeeService;
+    private final EmployeeService employeeService;
     private final GoogleService googleService;
 
     @Autowired
-    public EmployeeJob(EmployeeRepository employeeRepository, com.entropyteam.entropay.employees.services.EmployeeService employeeService, GoogleService googleService) {
+    public EmployeeJob(EmployeeRepository employeeRepository, EmployeeService employeeService, GoogleService googleService) {
         this.employeeRepository = employeeRepository;
         this.employeeService = employeeService;
         this.googleService = googleService;
