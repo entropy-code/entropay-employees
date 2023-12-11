a0e6af8e7b441f7b6cb8e5ad49a7b9428f34f756
Javier Paez <89614950+Paez99@users.noreply.github.com>
2023-12-11 10:58:53 -0300

    EN-117: Filtering employees report by active employees (#133)
    * EN-117: Filtering employees report by active employees
    
    * EN-117: Changes after comments on the pr, changed the way the filter works, and changed param name for when an employee is active
    
    * EN-117: Removed unused imports
    
    * EN-117: improved the getfilteredemployees list method

--- src/main/java/com/entropyteam/entropay/employees/repositories/EmployeeRepository.java
+++ src/main/java/com/entropyteam/entropay/employees/repositories/EmployeeRepository.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import com.entropyteam.entropay.common.BaseRepository;
 import com.entropyteam.entropay.employees.models.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends BaseRepository<Employee, UUID> {
 
@@ -12,4 +14,7 @@ public interface EmployeeRepository extends BaseRepository<Employee, UUID> {
 
     List<Employee> findAllByDeletedIsFalseAndActiveIsTrue();
 
+    @Query(value = "SELECT e.* FROM Employee AS e INNER JOIN Contract AS c ON e.id = c.employee_id WHERE c.active = true AND c.deleted = false AND e.deleted = false",
+            nativeQuery = true)
+    List<Employee> getEmployeesWithAtLeastAnActiveContract();
 }
