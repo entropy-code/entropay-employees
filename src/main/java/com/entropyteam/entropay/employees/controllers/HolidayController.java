0ad5f921efa00f23d36e62f87ba509a858af0f92
FedericoA15 <federicoalmeida15@gmail.com>
2023-12-20 16:37:35 -0300

    EN-138: Add Holidays per country in shared google calendar

--- src/main/java/com/entropyteam/entropay/employees/controllers/HolidayController.java
+++ src/main/java/com/entropyteam/entropay/employees/controllers/HolidayController.java
@@ -2,21 +2,25 @@ package com.entropyteam.entropay.employees.controllers;
 
 import com.entropyteam.entropay.common.BaseController;
 import com.entropyteam.entropay.employees.dtos.HolidayDto;
+import com.entropyteam.entropay.employees.jobs.HolidayJob;
 import com.entropyteam.entropay.employees.services.HolidayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_DEVELOPMENT;
 
 @RestController
 @CrossOrigin
@@ -25,16 +29,32 @@ import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
 public class HolidayController extends BaseController<HolidayDto, UUID> {
 
     private final HolidayService holidayService;
+    private final HolidayJob holidayJob;
 
     @Autowired
-    public HolidayController(HolidayService holidayService) {
+    public HolidayController(HolidayService holidayService, HolidayJob holidayJob) {
         super(holidayService);
         this.holidayService = holidayService;
+        this.holidayJob = holidayJob;
     }
+
     @GetMapping("/years")
     public ResponseEntity<List<Map<String, Integer>>> getHolidayYears() {
         List<Map<String, Integer>> holidayYears = holidayService.getHolidayYears();
         return ResponseEntity.ok().header(BaseController.X_TOTAL_COUNT, String.valueOf(holidayYears.size()))
                 .body(holidayYears);
+    }
+
+    @GetMapping("/sync-holiday")
+    @Secured({ROLE_ADMIN, ROLE_DEVELOPMENT})
+    @Transactional
+    public ResponseEntity<String> syncHolidayWithCalendar() {
+        try {
+            holidayJob.syncEmployeesBirthdayWithCalendar();
+            return ResponseEntity.ok("Successful job execution: sync holiday");
+        } catch (IOException exception) {
+            String errorMessage = "An error occurred: " + exception.getMessage();
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
     }
 }
--- src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
+++ src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
@@ -3,6 +3,7 @@ package com.entropyteam.entropay.employees.repositories;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
 import com.entropyteam.entropay.common.BaseRepository;
 import com.entropyteam.entropay.employees.models.Holiday;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,8 @@ import org.springframework.data.jpa.repository.Query;
 public interface HolidayRepository extends BaseRepository<Holiday, UUID> {
 
     List<Holiday> findAllByDateBetweenAndDeletedFalseOrderByDateAsc(LocalDate startDate, LocalDate endDate);
+
+    List<Holiday> findAllByDeletedFalseAndYear(int year);
 
     @Query(value = "SELECT DISTINCT extract('Year' FROM date) AS year FROM holiday_calendar WHERE deleted=false "
             + " ORDER BY year ASC", nativeQuery = true)
