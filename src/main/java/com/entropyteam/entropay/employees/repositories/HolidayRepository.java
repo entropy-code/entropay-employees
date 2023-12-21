f1bf7e2ec3c455b306573805a6ebdcabf89f32c1
Federico Almeida <96883646+FedericoA15@users.noreply.github.com>
2023-12-21 17:40:25 -0300

    EN-138: Add Holidays per country in shared google calendar (#140)
    * EN-138: Add Holidays per country in shared google calendar
    
    * EN-138: fix repository, service and job

--- src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
+++ src/main/java/com/entropyteam/entropay/employees/repositories/HolidayRepository.java
@@ -3,13 +3,18 @@ package com.entropyteam.entropay.employees.repositories;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
 import com.entropyteam.entropay.common.BaseRepository;
 import com.entropyteam.entropay.employees.models.Holiday;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HolidayRepository extends BaseRepository<Holiday, UUID> {
 
     List<Holiday> findAllByDateBetweenAndDeletedFalseOrderByDateAsc(LocalDate startDate, LocalDate endDate);
+
+    @Query("SELECT h FROM Holiday h WHERE h.deleted = false AND YEAR(h.date) = :year")
+    List<Holiday> findAllByDeletedFalseAndYear(@Param("year") int year);
 
     @Query(value = "SELECT DISTINCT extract('Year' FROM date) AS year FROM holiday_calendar WHERE deleted=false "
             + " ORDER BY year ASC", nativeQuery = true)
