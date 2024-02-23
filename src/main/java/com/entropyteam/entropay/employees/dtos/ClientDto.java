9b42b9765919ad818cdffff858fa7ad2781e456b
Agustin Bourgeois <agustinb4@gmail.com>
2024-02-23 11:48:02 -0300

    En 149: Enlarge Dtos response for export function (#170)
    EN-149: enlage Dtos response

--- src/main/java/com/entropyteam/entropay/employees/dtos/ClientDto.java
+++ src/main/java/com/entropyteam/entropay/employees/dtos/ClientDto.java
@@ -21,15 +21,19 @@ public record ClientDto(UUID id,
                         String contactFullName,
                         String contactEmail,
                         String preferredCurrency,
-                        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
-                        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime modifiedAt,
+                        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                        LocalDateTime createdAt,
+                        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                        LocalDateTime modifiedAt,
+                        String companyName,
                         String internalId,
                         @NotNull(message = "Active state is mandatory")
                         boolean active) {
 
     public ClientDto(Client client) {
         this(client.getId(), client.getCompanyId(), client.getName(), client.getAddressLine(), client.getZipCode(),
-                client.getCity(), client.getState(), client.getCountry(), client.getContactFullName(), client.getContactEmail(),
-                client.getPreferredCurrency(), client.getCreatedAt(), client.getModifiedAt(), client.getInternalId(), client.isActive());
+                client.getCity(), client.getState(), client.getCountry(), client.getContactFullName(),
+                client.getContactEmail(), client.getPreferredCurrency(), client.getCreatedAt(), client.getModifiedAt(),
+                client.getCompany().getName(), client.getInternalId(), client.isActive());
     }
 }
