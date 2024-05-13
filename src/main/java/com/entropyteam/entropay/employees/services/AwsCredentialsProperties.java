55f83c0753ccd4a93a14440f36f6245deeb68238
Esteban Azaretzky <eazaretzky@gmail.com>
2024-05-13 10:26:25 -0300

    Removing lombok and mapstruct dependencies (#203)

--- src/main/java/com/entropyteam/entropay/employees/services/AwsCredentialsProperties.java
+++ src/main/java/com/entropyteam/entropay/employees/services/AwsCredentialsProperties.java
@@ -1,12 +1,34 @@
 package com.entropyteam.entropay.employees.services;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("aws-credentials")
-@Data
 public class AwsCredentialsProperties {
     private String accessKeyId;
     private String secretAccessKey;
     private String bucketName;
+
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+
+    public String getSecretAccessKey() {
+        return secretAccessKey;
+    }
+
+    public void setSecretAccessKey(String secretAccessKey) {
+        this.secretAccessKey = secretAccessKey;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
 }
--- src/main/java/com/entropyteam/entropay/employees/services/GoogleCredentialsProperties.java
+++ src/main/java/com/entropyteam/entropay/employees/services/GoogleCredentialsProperties.java
@@ -1,10 +1,8 @@
 package com.entropyteam.entropay.employees.services;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("google-credentials")
-@Data
 public class GoogleCredentialsProperties {
     private String calendarId;
     private String type;
@@ -33,6 +31,102 @@ public class GoogleCredentialsProperties {
                 "\"client_x509_cert_url\": \"" + clientX509CertUrl + "\",\n" +
                 "\"universe_domain\": \"" + universeDomain + "\"}";
         return propertiesString.getBytes();
+    }
+
+    public String getCalendarId() {
+        return calendarId;
+    }
+
+    public void setCalendarId(String calendarId) {
+        this.calendarId = calendarId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getPrivateKeyId() {
+        return privateKeyId;
+    }
+
+    public void setPrivateKeyId(String privateKeyId) {
+        this.privateKeyId = privateKeyId;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getAuthUri() {
+        return authUri;
+    }
+
+    public void setAuthUri(String authUri) {
+        this.authUri = authUri;
+    }
+
+    public String getTokenUri() {
+        return tokenUri;
+    }
+
+    public void setTokenUri(String tokenUri) {
+        this.tokenUri = tokenUri;
+    }
+
+    public String getAuthProviderX509CertUrl() {
+        return authProviderX509CertUrl;
+    }
+
+    public void setAuthProviderX509CertUrl(String authProviderX509CertUrl) {
+        this.authProviderX509CertUrl = authProviderX509CertUrl;
+    }
+
+    public String getClientX509CertUrl() {
+        return clientX509CertUrl;
+    }
+
+    public void setClientX509CertUrl(String clientX509CertUrl) {
+        this.clientX509CertUrl = clientX509CertUrl;
+    }
+
+    public String getUniverseDomain() {
+        return universeDomain;
+    }
+
+    public void setUniverseDomain(String universeDomain) {
+        this.universeDomain = universeDomain;
     }
 }
 
