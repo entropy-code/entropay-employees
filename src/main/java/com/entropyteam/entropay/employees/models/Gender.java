1ee56ba26f54ab69e67417ad9cb53e4f466321c3
Joa Fernandez Siles <joaquinfsiles@gmail.com>
2024-08-05 18:00:35 -0300

    small correction for Gender enum

--- src/main/java/com/entropyteam/entropay/employees/models/Gender.java
+++ src/main/java/com/entropyteam/entropay/employees/models/Gender.java
@@ -3,5 +3,5 @@ package com.entropyteam.entropay.employees.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @JsonFormat(shape = JsonFormat.Shape.STRING)
-public enum Gender{MALE, FEMALE, NO_BINARY}
+public enum Gender{MALE, FEMALE, NON_BINARY}
 
