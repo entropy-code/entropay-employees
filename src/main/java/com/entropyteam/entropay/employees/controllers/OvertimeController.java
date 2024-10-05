9dfbd4636cd27b759c42ff77dba6b549db31a263
Esteban Azaretzky <eazaretzky@gmail.com>
2024-10-05 11:12:11 -0300

    overtime logic (#276)
    Co-authored-by: eazaretzky <esteban@entropyteam.com>

---
+++ src/main/java/com/entropyteam/entropay/employees/controllers/OvertimeController.java
@@ -0,0 +1,31 @@
+package com.entropyteam.entropay.employees.controllers;
+
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_ADMIN;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_DEVELOPMENT;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_HR_DIRECTOR;
+import static com.entropyteam.entropay.auth.AuthConstants.ROLE_MANAGER_HR;
+
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.entropyteam.entropay.common.BaseController;
+import com.entropyteam.entropay.employees.dtos.OvertimeDto;
+import com.entropyteam.entropay.employees.services.OvertimeService;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/overtimes", produces = MediaType.APPLICATION_JSON_VALUE)
+@Secured({ROLE_ADMIN, ROLE_HR_DIRECTOR, ROLE_MANAGER_HR, ROLE_DEVELOPMENT})
+
+public class OvertimeController extends BaseController<OvertimeDto, UUID> {
+
+    @Autowired
+    public OvertimeController(OvertimeService overtimeService) {
+        super(overtimeService);
+    }
+
+}
