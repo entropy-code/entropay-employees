01d52850f69bb8b44ba5cb8c63f17080c0d83b3e
Esteban Azaretzky <eazaretzky@gmail.com>
2025-01-24 14:31:50 -0300

    Changes to get the roles from the authentication microservice (#316)
    Update to avoid using deprecated methods in WebSecurityConfig
    Changes to get the roles from the authentication microservice
    
    Co-authored-by: eazaretzky <esteban@entropyteam.com>

---
+++ src/main/java/com/entropyteam/entropay/auth/AuthorizationService.java
@@ -0,0 +1,51 @@
+package com.entropyteam.entropay.auth;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class AuthorizationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationService.class);
+    private final RestClient restClient;
+
+    public AuthorizationService(RestClient.Builder restClientBuilder, @Value("${user-auth-api-url}") String baseUrl) {
+        this.restClient = restClientBuilder.baseUrl(baseUrl).build();
+    }
+
+    public record Identity(UUID id, String username, String firstName, String lastName, String email,
+                           Map<UUID, Set<String>> rolesByTenant) {
+
+    }
+
+    /**
+     * Get the identity of the user with the given JWT.
+     *
+     * @param jwt The JWT to use for authentication.
+     * @return The identity of the user.
+     */
+    public Identity identity(String jwt) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Identity identity = this.restClient.get()
+                .uri("/auth/identity")
+                .header("authorization", "Bearer " + jwt)
+                .retrieve()
+                .body(Identity.class);
+
+        stopWatch.stop();
+        LOGGER.info("Finish getting identity for user: {}, took: {}ms", identity.email(),
+                stopWatch.getTotalTimeMillis());
+
+        return identity;
+    }
+
+}
