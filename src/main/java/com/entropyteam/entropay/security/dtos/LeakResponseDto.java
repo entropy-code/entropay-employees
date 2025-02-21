bbea8004bf1c7f98147198443489217f8ddf699f
Juan José Muñoz <juanjosemunoz@Juans-MacBook-Pro.local>
2025-02-21 18:44:51 -0300

    Unit testing and converting dtos in records, and some refactor

--- src/main/java/com/entropyteam/entropay/security/dtos/LeakResponseDto.java
+++ src/main/java/com/entropyteam/entropay/security/dtos/LeakResponseDto.java
@@ -4,13 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class LeakResponseDto {
-    private boolean success;
-    private List<LeakDto> result;
-
-    public boolean isSuccess() { return success; }
-    public void setSuccess(boolean success) { this.success = success; }
-
-    public List<LeakDto> getResult() { return result; }
-    public void setResult(List<LeakDto> result) { this.result = result; }
-}
+public record LeakResponseDto(
+        boolean success,
+        List<LeakDto> result
+) {}
--- src/main/java/com/entropyteam/entropay/security/dtos/SourceDto.java
+++ src/main/java/com/entropyteam/entropay/security/dtos/SourceDto.java
@@ -4,14 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SourceDto {
-    private String name;
-    @JsonProperty("breach_date")
-    private String breachDate;
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getBreachDate() { return breachDate; }
-    public void setBreachDate(String breachDate) { this.breachDate = breachDate; }
-}
+public record SourceDto(
+        String name,
+        @JsonProperty("breach_date") String breachDate
+) {}
